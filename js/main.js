4f7e6cdfdfb0452ac6f5741ad6919018b445fbdf
antoduarte <antoduarte192212@gmail.com>
2021-05-02 15:30:58 -0600

    swiper js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+});
